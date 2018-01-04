25f1546b1e34ad9c356eec43b241923a6246c8ba
Tyler D <drelyt@gmail.com>
2018-01-04 15:20:55 -0500

    leap year check working

--- calendar.py
+++ calendar.py
@@ -1,11 +1,29 @@
 
+import datetime
+
 class Calendar():
 
-    def is_leapyear(self, year):
+    def __init__(self, year):
+
+        self.year = year
+
+        #number of days per month
+        self.days_per_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
         
-        if year % 4 == 0 and year % 400 == 0:
-            if year % 100 == 0:
-                return False
-            else:
-                return True
+        #check if a leap year
+        if self.is_leapyear(year):
+            self.days_per_month[1] + 1
+
+        for month in range(0,12):
+            current_date = datetime.date(year,month + 1, 1)
+
+            print(current_date.strftime("%B: " + str(self.days_per_month[month])))
+
+    def is_leapyear(self, year):        
+        if (year % 4 == 0 or year % 400 == 0) and (year % 100 != 0):     
+            return True
+        else:
+            return False
+    
+
 
---
+++ main.py
@@ -0,0 +1,8 @@
+import calendar
+
+def main():
+    cal = calendar.Calendar(2018)
+
+
+if __name__ == "__main__":
+    main()
